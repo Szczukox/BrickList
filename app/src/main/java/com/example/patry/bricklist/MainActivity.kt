fbdaa2d34d1a317b154108b494138d78741bfb1a
Szczukox <33254494+Szczukox@users.noreply.github.com>
2018-06-03 16:46:33 +0200

    Add database and update Main Activity

--- app/src/main/java/com/example/patry/bricklist/MainActivity.kt
+++ app/src/main/java/com/example/patry/bricklist/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.example.patry.bricklist
 
+import android.database.SQLException
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val DBHelper = DataBaseHelper(this)
+        try {
+
+            DBHelper.createDataBase()
+
+        } catch (ioe: IOException) {
+
+            throw Error("Unable to create database")
+        }
+        try {
+
+            DBHelper.openDataBase()
+
+        } catch (sqle: SQLException) {
+
+            throw sqle
+
+        }
+
     }
 }
